82176883db31df0fbddda8e06ef59125333b9e88
Tsierenana Bôtramanagna Gracy <gtsierenana@gmail.com>
2023-07-22 16:55:03 +0300

    fix: Non-typed issu

--- src/components/CustomPointer.tsx
+++ src/components/CustomPointer.tsx
@@ -1,22 +1,18 @@
 import { useEffect, useRef } from "react"
 
 export default function CustomPointer() {
-    const delay = 18;
-
     const endX = useRef(window.innerWidth / 2);
     const endY = useRef(window.innerHeight / 2);
-
-    const requestRef = useRef(null);
 
     const mouseMoveEvent = (e: MouseEvent) => {
         endX.current = e.pageX;
         endY.current = e.pageY;
 
-        customPointer.current.style.top = endY.current + 'px';
-        customPointer.current.style.left = endX.current + 'px';
+        customPointer.current!.style.top = `${endY.current} px`;
+        customPointer.current!.style.left = `${endX.current} px`;
     }
 
-    const customPointer = useRef(null);
+    const customPointer = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         document.addEventListener('mousemove', mouseMoveEvent);
