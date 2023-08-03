229f91513c2781b89787d2665cdc68f955818d75
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2023-08-03 16:13:24 +0300

    add atropos

--- src/components/ProjectSection/ProjectCard.tsx
+++ src/components/ProjectSection/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import { FaGithub, FaArrowRight } from "react-icons/fa";
 import StackSet from "./StackSet";
+import Atropos from "atropos/react";
 
 type Project = {
     name: string,
@@ -11,19 +12,21 @@ type Project = {
 
 export default function ProjectCard({ project }: { project: Project }) {
     return (
-        <article className="project-card">
-            <h3>{project.name}</h3>
-            <div className="project-img-container">
-                <img src={project.image} />
-            </div>
-            <p>{project.description}</p>
-            <div className="footer">
-                <StackSet data={project.stack} />
-                <ul className="links">
-                    <li title="Voir détails"><a href="#"><FaArrowRight /></a></li>
-                    <li title="Lien github"><a href={project.github}><FaGithub /></a></li>
-                </ul>
-            </div>
-        </article>
+        <Atropos shadow={false}>
+            <article className="project-card">
+                <h3>{project.name}</h3>
+                <div className="project-img-container">
+                    <img src={project.image} />
+                </div>
+                <p>{project.description}</p>
+                <div className="footer">
+                    <StackSet data={project.stack} />
+                    <ul className="links">
+                        <li title="Voir détails"><a href="#"><FaArrowRight /></a></li>
+                        <li title="Lien github"><a href={project.github}><FaGithub /></a></li>
+                    </ul>
+                </div>
+            </article>
+        </Atropos>
     );
 }
