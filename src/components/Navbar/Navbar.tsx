c93850ff0270a1016353b701c397a7b9e0303f24
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2024-01-24 17:40:22 +0300

    ui: Responsive navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Menu } from "lucide-react";
+
 import Logo from "../Navbar/Logo";
 import Navlink from "../Navbar/Navlink";
 
@@ -5,12 +7,21 @@ export default function Navbar() {
     return (
         <nav>
             <Logo />
-            <ul>
+            <ul className="expanded-navbar">
                 <Navlink label={"About"} target={"#about"} />
                 <Navlink label={"Skills"} target={"#skills"} />
                 <Navlink label={"Projects"} target={"#projects"} />
                 <Navlink label={"Contact"} target={"#contact"} />
             </ul>
+
+            <div className="shrinked-navbar"><Menu />
+                <ul className="hamburger-navbar">
+                    <Navlink label={"About"} target={"#about"} />
+                    <Navlink label={"Skills"} target={"#skills"} />
+                    <Navlink label={"Projects"} target={"#projects"} />
+                    <Navlink label={"Contact"} target={"#contact"} />
+                </ul>
+            </div>
         </nav>
     );
 }
