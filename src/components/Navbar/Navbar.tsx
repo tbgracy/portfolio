e352343b4b019b255f1303a23ebb8fcdb2e0841b
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2024-05-11 18:59:50 +0300

    fix hamburger menu display

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,8 +2,19 @@ import { Menu } from "lucide-react";
 
 import Logo from "../Navbar/Logo";
 import Navlink from "../Navbar/Navlink";
+import { useRef } from "react";
 
 export default function Navbar() {
+    const navRef = useRef<HTMLUListElement>(null);
+
+    function handleClick() {
+        if (navRef.current!.classList.contains('show')) {
+            navRef.current!.classList.remove('show');
+        } else {
+            navRef.current!.classList.add('show');
+        }
+    }
+
     return (
         <nav>
             <Logo />
@@ -14,8 +25,8 @@ export default function Navbar() {
                 <Navlink label={"Contact"} target={"#contact"} />
             </ul>
 
-            <div className="shrinked-navbar"><Menu />
-                <ul className="hamburger-navbar">
+            <div className="shrinked-navbar" onClick={handleClick}><Menu />
+                <ul ref={navRef} className="hamburger-navbar">
                     <Navlink label={"About"} target={"#about"} />
                     <Navlink label={"Skills"} target={"#skills"} />
                     <Navlink label={"Projects"} target={"#projects"} />
